6b4c1dd326f871cb6e9fc56e4be972145bc1db18
Nguyễn Việt Anh <83152264+nVietUK@users.noreply.github.com>
2021-06-13 16:48:31 +0700

    forget XD

--- test.py
+++
@@ -1,6 +0,0 @@
-filename = 'FakeDiscord'
-settingfile = open(filename+'.txt', 'r',  encoding="utf-8")
-print(settingfile.readline().split(' ', 2)[2])
-print(settingfile.readline().split(' ', 2)[2])
-print(settingfile.readline().split(' ', 2)[2])
-print(settingfile.readline().split(' ', 2)[2])
